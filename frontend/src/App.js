f9c4b3f7583d24d936ca97844a739b3a6f1a7515
joaoclaudin0 <jclaudinodev@gmail.com>
2020-03-26 11:06:55 -0300

    Front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
+import Routes from './routes';
+
+import './global.css'
+
+function App() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Routes/>
     );
-  } 
+  
 }
 
 export default App;
